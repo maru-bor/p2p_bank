218611e4980ad729f5f364ea78144941a2005d3d
marie <126610455+maru-bor@users.noreply.github.com>
2026-01-19 11:45:24 +0100

    Changed method for fetching device IP address

--- utils.py
+++ utils.py
@@ -1,12 +1,9 @@
 import socket
 
 def get_own_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
     except Exception:
         ip = "127.0.0.1"
-    finally:
-        s.close()
     return ip
